898899c15de058cceadfb055308bc38f23458ad6
Dawson Hamilton <dawsondhamilton@gmail.com>
2020-04-18 09:03:47 -0600

    finishing up

--- src/components/Paragraph/index.js
+++ src/components/Paragraph/index.js
@@ -3,7 +3,13 @@ import "./style.css";
 
 function Paragraph() {
     return (
-        <h3 className="aboutMe">Just making sure this works. </h3>
+        <h3 className="aboutMe">Hello, my name is Dawson Hamilton and I am a full stack Web Developer. My skills include a variety
+        of coding languages but i have found myself to really enjoy front end languages like HTML, CSS, JavaScript, jQueary, React.js...
+        One of the things that I think learning about computer programming has tought me a lot about is how to work really well in
+        a team environment. I have also learned  alot about the processes of reasearching the answer to questions that I may have about
+        problems with my code. One my free time I really love to do really anything with computers whether that is coding, watching
+        youtube videos, playing video games, or even building computers themselves! Looking to really put myself out there and learn
+        the most that I can about computer programming. </h3>
     );
 }
 
--- src/pages/AboutMe.js
+++ src/pages/AboutMe.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Container } from "../components/Grid";
+import { Container, Row, Col } from "../components/Grid";
 import Title from "../components/Title";
 import Paragraph from "../components/Paragraph";
 
@@ -15,7 +15,14 @@ function AboutMe() {
                 blankLength="md-5"
                 length="md-2"
             />
-            <Paragraph />
+            <br />
+            <Row>
+                <Col size="md-2"></Col>
+                <Col size="md-8">
+                    <Paragraph />
+                </Col>
+                <Col size="md-2"></Col>
+            </Row>
         </Container >
     );
 }
