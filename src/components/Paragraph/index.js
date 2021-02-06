a4adda0f670295c3b35a3b781b11bbb87a9e6d5a
PRISON.MIKE <dawsondhamilton@gmail.com>
2021-02-05 17:42:53 -0700

    fixing css that was broken, updating resume, and also changing the about me paragragh a little

--- src/components/Paragraph/index.js
+++ src/components/Paragraph/index.js
@@ -5,11 +5,12 @@ function Paragraph() {
     return (
         <h3 className="aboutMe">Hello, my name is Dawson Hamilton and I am a full stack Web Developer. My skills include a variety
         of coding languages but i have found myself to really enjoy front end languages like HTML, CSS, JavaScript, jQueary, React.js...
-        One of the things that I think learning about computer programming has tought me a lot about is how to work really well in
-        a team environment. I have also learned  alot about the processes of reasearching the answer to questions that I may have about
-        problems with my code. One my free time I really love to do really anything with computers whether that is coding, watching
-        youtube videos, playing video games, or even building computers themselves! Looking to really put myself out there and learn
-        the most that I can about computer programming. </h3>
+        One of the things that computer programming has tought me a lot about is how to work really well in a team environment which I
+        feel is very important as I make my way into the job force. I have also learned  alot about the processes of reasearching the
+        answer to questions that I may have about problems with my code. I have also learned what it is like to work in a fast paced
+        environment which I feel like is a good skill to aquire. On my free time I really love to do really anything with computers
+        whether that is coding, watching youtube videos, playing video games, or even building computers themselves! Looking to really
+        put myself out there and learn the most that I can about computer programming. </h3>
     );
 }
 
